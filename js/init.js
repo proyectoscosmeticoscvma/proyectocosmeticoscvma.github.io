eda7a619ea4271c8af6e2af97902d48af743f01b
proyectoscosmeticoscvma <119483601+proyectoscosmeticoscvma@users.noreply.github.com>
2022-11-30 02:08:06 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -8,11 +8,11 @@
  * servidor. */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyDzZxk913MUi3ESjLwNK14W0t1c6HgUOWE",
-    authDomain: "bdfalumnox.firebaseapp.com",
-    projectId: "bdfalumnox",
-    storageBucket: "bdfalumnox.appspot.com",
-    messagingSenderId: "1002536188546",
-    appId: "1:1002536188546:web:cfa9fb33aec119e7fc529b",
-    measurementId: "G-5HHSLWQ24J"
+    apiKey: "AIzaSyARA-h_lYyAsgiWfRrqvLwqdzWl0nGUvPA",
+  authDomain: "proyectocosmeticoscvma0.firebaseapp.com",
+  projectId: "proyectocosmeticoscvma0",
+  storageBucket: "proyectocosmeticoscvma0.appspot.com",
+  messagingSenderId: "224500558523",
+  appId: "1:224500558523:web:4c52ef020811b494ab579e",
+  measurementId: "G-2VY4F9MFK3"
 });
